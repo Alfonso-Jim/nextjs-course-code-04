839917984e7bc3f98042658cbf85e74a2ea7d521
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-07-05 09:52:27 +0200

    Add getStaticProps fn to index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 function HomePage(props) {
+  const { data } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {data.map((item) => (
+        <li key={item.id}>{item.name}</li>
+      ))}
     </ul>
   );
 }
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      data: [
+        { id: '01', name: 'example1' },
+        { id: '02', name: 'example2' },
+      ],
+    },
+  };
+};
 
 export default HomePage;
