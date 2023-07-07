5f35ec4a5fdcf8d7a3804caa6b908001bb0c9a28
alfonsojim1897 <alfonsojim1897@gmail.com>
2023-07-07 13:15:03 +0200

    Add fallback and ts definitions

--- pages/[productId].tsx
+++ pages/[productId].tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import fs from 'fs/promises';
 import path from 'path';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { Product, Products } from '../interfaces/interfaces';
 
-const ProductDetailPage = (props) => {
+const ProductDetailPage: NextPage<Product> = (props) => {
   const { loadedProduct } = props;
-
   if (!loadedProduct) {
     return <p>Loading...</p>;
   }
@@ -19,16 +20,22 @@ const ProductDetailPage = (props) => {
 
 const getData = async () => {
   const filePath = path.join(process.cwd(), 'data', 'dummy-data.json');
-  const jsonData = await fs.readFile(filePath);
+  const jsonData = await fs.readFile(filePath, 'utf8');
   const data = JSON.parse(String(jsonData));
-  return data;
+  return data as Products;
 };
 
-export const getStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   const { params } = context;
   const productId = params.productId;
   const data = await getData();
   const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
@@ -37,10 +44,11 @@ export const getStaticProps = async (context) => {
   };
 };
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const data = await getData();
   const productIds = data.products.map((product) => product.id);
   const pathsWithParams = productIds.map((id) => ({ params: { productId: id } }));
+
   return {
     paths: pathsWithParams,
     // if we use fallback:'blocking' then next will take a bit more to load the page
@@ -48,7 +56,7 @@ export const getStaticPaths = async () => {
     // if we use fallback true then we can set a spinner while we wait for the page to load
     // fallback: true,
     // set to false because we load all productId's
-    fallback: false,
+    fallback: true,
   };
 };
 
